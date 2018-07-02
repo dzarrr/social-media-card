78e46777443b2486e5ecbe34f4b0c7cf15558ee8
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2018-07-02 17:48:37 +0700

    add Card components

---
+++ social-card/src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const OuterContainer = styled.div`
+  height: 100px;
+  width: 200px;
+  background-color: #ffffff;
+`;
+
+const Card = () => (
+  <OuterContainer>
+    Ini adalah latihan saya demi memperoleh kebahagian bersamanya
+  </OuterContainer>
+);
+
+
+export default Card;
