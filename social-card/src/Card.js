e32f0d01b3a54f1dd6b932d51b8435186672188f
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2018-07-03 17:48:47 +0700

    add ImageWrapper and DescriptionContainer

--- social-card/src/Card.js
+++ social-card/src/Card.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import cardImage from './images/img1.png';
 
-const OuterContainer = styled.div`
-  height: 100px;
-  width: 200px;
+const Container = styled.div`
+  height: auto;
+  width: 100%;
   background-color: #ffffff;
 `;
 
+const ImageWrapper = styled.div`
+  height: 200px;
+  width: 500px;
+`;
+
+const DescriptionContainer = styled.div`
+  width: 500px;
+  height: auto;
+`;
+
+const StyledLink = styled.a`
+`;
+
+const CardImage = styled.img`
+  width: 500px;
+  height: 200px;
+`;
+
 const Card = () => (
-  <OuterContainer>
-    Ini adalah latihan saya demi memperoleh kebahagian bersamanya
-  </OuterContainer>
+  <Container>
+    <ImageWrapper>
+      <CardImage src={cardImage} />
+    </ImageWrapper>
+    <DescriptionContainer>
+      <b> Learning React? Start Small. </b>
+      <br />
+      Can't pry away from the tutorials ? The cure is to make tiny little experiment apps.
+      <br />
+      <StyledLink href='dev.to'> dev.to </StyledLink>
+    </DescriptionContainer>
+  </Container>
 );
 
 
